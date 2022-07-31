996c913b2f5b86e598c62c6335342e85eacde100
yaluvv <yaluvv@yandex.ru>
2022-07-31 22:15:00 +0300

    added anchor links for footer, burger navigation

--- js/app.js
+++ js/app.js
@@ -1,16 +1,21 @@
 const nav = document.querySelector("#nav");
 const navMenu = document.querySelector("#nav-burger");
 const navMenuImage = document.querySelector("#nav-burger-image");
-
-
-// navMenu.onClick = () => {
-//    
-// }
+const navLinks = document.querySelectorAll('.nav-link')
+const body = document.body
 
 navMenu.onclick = () => {
     if (nav.classList.toggle('open')) {
         navMenuImage.src = './icons/burger-close.svg'
+        body.classList.add('scroll-hidden')
     } else {
         navMenuImage.src = './icons/burger-open.svg'
     }
 }
+
+navLinks.forEach(n => n.addEventListener('click', () => {
+    nav.classList.remove('open')
+    navMenuImage.src = './icons/burger-open.svg'
+}))
+
+
